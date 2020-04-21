c35af9a47720f33398b9a055093894df64858649
Gervan <gervan@users.noreply.github.com>
2020-04-20 22:16:58 -0400

    latest updates to the site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
+import { MenComponent } from './men/men.component';
+import { WomenComponent } from './women/women.component';
+import { KidsComponent } from './kids/kids.component';
+import { HomeComponent } from './home/home.component';
+import { NavLeftComponent } from './nav-left/nav-left.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavMenuComponent
+    NavMenuComponent,
+    MenComponent,
+    WomenComponent,
+    KidsComponent,
+    HomeComponent,
+    NavLeftComponent
   ],
   imports: [
     BrowserModule,
